6581cb65a8ee2e161c02daa90d53b979b904e379
Soma Nyiro <nysoma@protonmail.com>
2023-02-24 11:30:53 +0100

    made dancers individual objects

--- AnimationScene.cs
+++ AnimationScene.cs
@@ -6,11 +6,12 @@ namespace Tests
 {
 public class AnimationScene
 {
+
     public static void Test()
     {
         InitWindow(1280, 720, "Animation test");
         
-        SetTargetFPS(60);
+        //SetTargetFPS(60);
 
         Camera3D camera;
         camera.position = new Vector3(10.0f, 10.0f, 10.0f);
@@ -21,34 +22,41 @@ public class AnimationScene
 
         SetCameraMode(camera, CameraMode.CAMERA_FREE);
 
-        Model dancer = LoadModel("resources/models/dance.iqm");
-        uint animsCount = 0;
-        var anims = LoadModelAnimations("resources/models/dance.iqm", ref animsCount);
-        int animFrameCounter = 0;
-        Texture2D dancerTexture = LoadTexture("resources/models/danceTexture.png");
-        SetMaterialTexture(ref dancer, 0, MaterialMapIndex.MATERIAL_MAP_ALBEDO, ref dancerTexture);
+        int gridSize = 3;
+        float separation = 5;
+        List<Dancer> dancers = new List<Dancer>();
+
+        for (int i = 0; i < gridSize; i++)
+        
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                dancers.Add(new Dancer(
+                    "resources/models/dancer_mid.iqm",
+                    "resources/models/danceTexture.png",
+                    24,
+                    new Vector3(i*separation, 0, j*separation),
+                    0.05f));
+            }
+        }
+
 
 
         while (!WindowShouldClose())
         {
             UpdateCamera(ref camera);
-            
-            animFrameCounter++;
-            UpdateModelAnimation(dancer, anims[0], animFrameCounter);
-            if (animFrameCounter >= anims[0].frameCount)
-                animFrameCounter = 0;
+
+            float delta = GetFrameTime();
             
             BeginDrawing();
             ClearBackground(Color.RAYWHITE);
 
             BeginMode3D(camera);
 
-            for (int x = 0; x < 20; x++)
+            foreach (var item in dancers)
             {
-                for (int y = 0; y < 20; y++)
-                {
-                    DrawModel(dancer, new Vector3(x*10, 0, y*10), 0.05f, Color.WHITE);
-                }
+                item.AdvanceAnimation(delta);
+                item.DrawDancer();
             }
 
             EndMode3D();
@@ -60,5 +68,62 @@ public class AnimationScene
 
         CloseWindow();
     }
+
 }
+
+class Dancer
+{
+    Model mesh;
+    float animFramesPerSec = 1;
+    uint numberOfAnims = 0;
+    int currentAnimFrame = 0;
+    float timeSinceLastAnimFrame = 0;
+    Texture2D texture;
+    List<ModelAnimation> animations = new List<ModelAnimation>();
+    Vector3 position = Vector3.Zero;
+    float scale = 1;
+
+    public Dancer(string modelPath, string texturePath, float animSpeed, Vector3 position, float scale)
+    {
+        this.position = position;
+        this.scale = scale;
+
+        mesh = LoadModel(modelPath);
+        texture = LoadTexture(texturePath);
+        SetMaterialTexture(ref mesh, 0, MaterialMapIndex.MATERIAL_MAP_ALBEDO, ref texture);
+
+        var animsSpan = LoadModelAnimations(modelPath, ref numberOfAnims);
+        foreach (var item in animsSpan)
+        {
+            animations.Add(item);
+        }
+
+        animFramesPerSec = animSpeed;
+    }
+
+    public void DrawDancer()
+    {
+        DrawModel(mesh, position, scale, Color.WHITE);
+    }
+
+    public void AdvanceAnimation(float delta)
+    {
+        timeSinceLastAnimFrame += delta;
+        bool shouldUpdate = false;
+
+        while (timeSinceLastAnimFrame > 1 / animFramesPerSec)
+        {
+            shouldUpdate = true;
+            timeSinceLastAnimFrame -= 1 / animFramesPerSec;
+            currentAnimFrame++;
+            if (currentAnimFrame >= animations[0].frameCount)
+                currentAnimFrame = 0;
+        } 
+
+        if (shouldUpdate) UpdateModelAnimation(mesh, animations[0], currentAnimFrame);
+
+    }
+
+}
+
 }
